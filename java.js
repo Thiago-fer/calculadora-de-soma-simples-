8757619f82e46f6d680dc25817ac358de80c00f4
Thiago-fer <157428308+Thiago-fer@users.noreply.github.com>
2024-03-27 20:47:16 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+var n1 = document.querySelector('#n1')
+var n2 = document.querySelector('#n2')
+var Result = document.querySelector('span')
+function somar(){
+    Result.innerHTML= parseInt(n1.value) + parseInt(n2.value)
+}
